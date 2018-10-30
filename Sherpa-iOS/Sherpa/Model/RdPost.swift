00c4ac415bec91b27170825dad040e4d6578cf1a
Nick Reichard <reich452@regis.edu>
2018-10-30 13:08:41 -0600

    fixed folders

---
+++ Sherpa-iOS/Sherpa/Model/RdPost.swift
@@ -0,0 +1,30 @@
+//
+//  RdPost.swift
+//  Sherpa
+//
+//  Created by Nick Reichard on 10/30/18.
+//  Copyright © 2018 Nick Reichard. All rights reserved.
+//
+
+import Foundation
+struct JsonDictionary: Decodable {
+    
+    let data: DataDictionary
+}
+
+struct DataDictionary: Decodable {
+    
+    let children: [PostDictionary]
+    let after: String?
+    
+    struct PostDictionary: Decodable {
+        let data: RDPost
+    }
+}
+
+struct RDPost: Decodable {
+    
+    let title: String?
+    let thumbnail: String?
+    let permalink: String?
+}
