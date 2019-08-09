9a71b8ebdcf8ed43e13e6ff20fed8b9ec8272443
Nick Reichard <reich452@regis.edu>
2019-08-09 12:28:53 -0600

    Finished voting feature

--- Sherpa-iOS/Sherpa/ModelController/VoteController.swift
+++ Sherpa-iOS/Sherpa/ModelController/VoteController.swift
@@ -7,3 +7,77 @@
 //
 
 import Foundation
+import Firebase
+
+enum DBVoteKey: String {
+    case firebaseCount
+    case cloudKitCount
+}
+
+class VoteController {
+    
+    static let shared = VoteController()
+    private init() { }
+    private let databaseReference = Database.database().reference()
+    private let userDefaults = UserDefaults.standard
+    
+    func didVoteForCloudKit() -> Bool {
+        if userDefaults.value(forKey: DBVoteKey.cloudKitCount.rawValue) as? Bool == true {
+            return true
+        }
+        return false
+    }
+    
+    func didVoteForFirebase() -> Bool {
+        if userDefaults.value(forKey: DBVoteKey.firebaseCount.rawValue) as? Bool == true {
+            return true
+        }
+        return false
+    }
+    
+    func fetchVoteCount(compeletion: @escaping (Result<Vote, Error>) -> Void) {
+        
+        let query = databaseReference.child("vote")
+        
+        query.observeSingleEvent(of: .value) { (snap) in
+            
+            guard let vote = Vote(snapShot: snap) else { compeletion(.failure(NetworkError.dataNotDecodable)); return }
+            compeletion(.success(vote))
+        }
+    }
+    
+    
+    // MARK: - Update
+    
+    func updateVote(dbVoteKey: DBVoteKey, compeletion: @escaping (Result<Bool, Error>) -> Void, didVoteCompletion: @escaping (String) -> Void) {
+        
+        if didVoteForCloudKit() == true || didVoteForFirebase() == true {
+            didVoteCompletion("You've Already Voted"); return 
+        }
+        
+        self.databaseReference.child("vote").child(dbVoteKey.rawValue).runTransactionBlock({ (mutableData) -> TransactionResult in
+            
+            if let data = mutableData.value as? Int {
+                mutableData.value = data + 1
+                return TransactionResult.success(withValue: mutableData)
+            } else {
+                return TransactionResult.success(withValue: mutableData)
+            }
+            
+        }, andCompletionBlock: { [weak self] (error, bool, snap) in
+            if let error = error {
+                print("Error updating vote count: \(#function) \(error) \(error.localizedDescription)")
+                compeletion(.failure(error))
+            }
+            if !bool {
+                print("Cannot Complete vote updates")
+            } else {
+                self?.userDefaults.set(true, forKey: dbVoteKey.rawValue)
+                compeletion(.success(true))
+            }
+            
+            }, withLocalEvents: false)
+    }
+}
+
+
--- Sherpa-iOS/Sherpa/ViewControllers/VoteViewController.swift
+++ Sherpa-iOS/Sherpa/ViewControllers/VoteViewController.swift
@@ -18,14 +18,17 @@ class VoteViewController: UIViewController {
     @IBOutlet weak var firebaseBtn: UIButton!
     @IBOutlet weak var voteLabel: UILabel!
     @IBOutlet weak var pieChartView: PieChartView!
+    @IBOutlet weak var ckVotedImageView: UIImageView!
+    @IBOutlet weak var fbVotedImageView: UIImageView!
     
     // MARK: - Properties
     
     var cloudKitEntry = PieChartDataEntry(value: 0)
     var firebaseEntry = PieChartDataEntry(value: 0)
     var voteDataEntries = [PieChartDataEntry]()
-    var cloudKitCounter: Double = 50.0
-    var firebaseCounter: Double = 50.0
+    var cloudKitCounter: Double = 1
+    var firebaseCounter: Double = 1
+    var vote: Vote?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,35 +44,91 @@ class VoteViewController: UIViewController {
         firebaseBtn.layer.cornerRadius = 15
         firebaseBtn.clipsToBounds = true
         
-        cloudKitEntry.value = cloudKitCounter
         cloudKitEntry.label = "CloudKit"
-        
-        firebaseEntry.value = firebaseCounter
         firebaseEntry.label = "Firebase"
-        voteDataEntries = [firebaseEntry, cloudKitEntry]
+        pieChartView.holeColor = UIColor.offsetBlack
+        ckVotedImageView.isHidden = true
+        fbVotedImageView.isHidden = true
     }
     
     func updateViews() {
-        let chartDataSet = PieChartDataSet(entries: voteDataEntries, label: nil)
-        let chartData = PieChartData(dataSet: chartDataSet)
-        
-        let colors = [UIColor.firebaseDarkOrange, UIColor.cloudKitLightBlue]
-        chartDataSet.colors = colors
-        pieChartView.data = chartData
+        checkVote()
+        VoteController.shared.fetchVoteCount { [weak self] (result) in
+            guard let self = self else { return }
+            switch result {
+            case .success(let vote):
+                self.vote = vote
+                self.cloudKitEntry.value = Double(vote.cloudKitCount)
+                self.firebaseEntry.value = Double(vote.firebaseCount)
+                self.voteDataEntries = [self.firebaseEntry, self.cloudKitEntry]
+                let chartDataSet = PieChartDataSet(entries: self.voteDataEntries, label: nil)
+                let chartData = PieChartData(dataSet: chartDataSet)
+                
+                let colors = [UIColor.firebaseDarkOrange, UIColor.cloudKitLightBlue]
+                chartDataSet.colors = colors
+                self.pieChartView.data = chartData
+            case .failure(let error):
+                self.showNoActionAlert(titleStr: "Error Getting Live Votes", messageStr: error.localizedDescription, style: .cancel)
+            }
+        }
+    }
+    
+    func checkVote() {
+        if VoteController.shared.didVoteForCloudKit() == true {
+            let sequence = AnimationSequence(withStepDuration: 0.5)
+            sequence.doStep {
+                self.titleLabel.text = "You voted for CloudKit"
+            }
+            sequence.execute()
+            let secondSq = AnimationSequence(withStepDuration: 0.6)
+            secondSq.execute()
+            sequence.onCompletion {
+                self.ckVotedImageView.isHidden = false
+            }
+            
+        } else if VoteController.shared.didVoteForFirebase() == true {
+            let sequence = AnimationSequence(withStepDuration: 0.5)
+            sequence.doStep {
+                self.titleLabel.text = "You voted for Firebase"
+            }
+            sequence.execute()
+            let secondSq = AnimationSequence(withStepDuration: 0.6)
+            secondSq.execute()
+            sequence.onCompletion {
+                self.fbVotedImageView.isHidden = false
+            }
+        }
     }
     
     // MARK: - Actions
     
     @IBAction func cloudKitBtnTapped(_ sender: Any) {
-        cloudKitCounter += 1
-        cloudKitEntry.value = cloudKitCounter
-        updateViews()
+        VoteController.shared.updateVote(dbVoteKey: .cloudKitCount, compeletion: { (result) in
+            switch result {
+            case .success(_):
+                print("A user updated")
+                self.updateViews()
+            case .failure(let error):
+                
+                self.showNoActionAlert(titleStr: "Error Updating Backend", messageStr: error.localizedDescription, style: .cancel)
+            }
+        }) { (message) in
+            self.showNoActionAlert(titleStr: message, messageStr: "Can't vote twice 🤓. Thanks for voting!", style: .cancel)
+        }
     }
     @IBAction func firebaseBtnTapped(_ sender: Any) {
-        firebaseCounter += 1
-        firebaseEntry.value = firebaseCounter
-        updateViews()
+        VoteController.shared.updateVote(dbVoteKey: .firebaseCount, compeletion: { (result) in
+            switch result {
+            case .success(_):
+                print("A user updated")
+                self.updateViews()
+            case .failure(let error):
+                
+                self.showNoActionAlert(titleStr: "Error Updating Backend", messageStr: error.localizedDescription, style: .cancel)
+            }
+        }) { (message) in
+            self.showNoActionAlert(titleStr: message, messageStr: "Can't vote twice 🤓. Thanks for voting!", style: .cancel)
+        }
     }
-    
     
 }
